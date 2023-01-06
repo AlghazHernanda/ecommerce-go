66151f9121bc7f3982b770ec3b31901c1214b9a4
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-01-06 11:05:59 +0700

    add package

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+// import (
+// 	"github.com/AlghazHernanda/ecommerce-go/controllers"
+// 	"github.com/AlghazHernanda/ecommerce-go/database"
+// 	"github.com/AlghazHernanda/ecommerce-go/middleware"
+// 	"github.com/AlghazHernanda/ecommerce-go/routes"
+// 	"github.com/gin-gonic/gin"
+// )
+
+func main() {
+
+}
