6eb80e4a7178dff6caad4a026984ba283f4e79e8
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-01-09 14:01:07 +0700

    add hashpashword logic

---
+++ controllers/controllers.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Panic(err)
+	}
+	return string(bytes)
+}
+
+func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
+
+}
