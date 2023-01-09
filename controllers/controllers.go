cbe07f91888b7fd29a24f9c2f5841f3a56a7bb02
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-01-09 14:04:16 +0700

    add another function

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -15,5 +16,17 @@ func HashPassword(password string) string {
 }
 
 func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
+
+}
+
+func SignUp() gin.HandlerFunc {
+
+}
+
+func Login() gin.HandlerFunc {
+
+}
+
+func ProductViewerAdmin() gin.HandlerFunc {
 
 }
