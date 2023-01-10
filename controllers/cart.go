3769986ccac80c38fef0453617a508793d6d973a
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-01-10 14:49:39 +0700

    add cart func

---
+++ controllers/cart.go
@@ -0,0 +1,17 @@
+package controllers
+
+func (app *Application) AddToCart() gin.Handler {
+}
+
+func (app *Application) RemoveItem() gin.HandlerFunc {
+}
+
+func GetItemFromCart() gin.HandlerFunc {
+
+}
+
+func (app *Application) BuyFromCart() gin.HandlerFunc {
+}
+
+func (app *Application) InstantBuy() gin.HandlerFunc {
+}
