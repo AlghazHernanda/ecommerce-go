c356999215b6ac276367db82a26f1ad20b5255b5
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-01-11 13:53:53 +0700

    add login logic

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -65,11 +66,67 @@ func SignUp() gin.HandlerFunc {
 			return
 		}
 
+		password := HashPassword(*user.Password)
+		user.Password = &password
+		user.Created_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.Update_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID_At = primitive.NewObjectID()
+		user.User_ID = user.ID.Hex()
+		token, refreshtoken, _ := generate.TokenGenerator(*user.Email, *user.First_Name, user.User_ID)
+		user.Token = &token
+		user, Refresh_Token = &refreshtoken
+		//make mean to create and here its an array
+		user.UserCart = make([]models.ProductUser, 0)
+		user.Address_Details = make([]models.Address, 0)
+		user.Order_Status = make([]models.OrderStatus, 0)
+		// capture all this inserted data into a the database
+		_, inserterr := UserCollection.InsertOne(ctx, user)
+		if inserterr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "the user did not get created"})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusCreated, "succesfully signed in :)")
+
 	}
 
 }
 
 func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var user models.User
+		if err := c.BIND_JSO(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
+		err := UserCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&founduser)
+		defer cancel()
+
+		if err != nil {
+			c.JSON(https.StatusInternalServerError, gin.H{"error": "login or passwor incorrect"})
+			return
+		}
+		PasswordIsVaild, msg := VerifyPassword(*user.Password.Password, *founduser.Password)
+
+		//delay the execution of the function or method or an anonymous method until the nearby functions returns. In other words, defer function
+		//or method call arguments evaluate instantly, but they don't execute until the nearby functions returns
+		defer cancel()
+
+		if !PasswordIsVaild {
+			c.JSON{http.StatusInternalServerError, gin.H{"error": msg}}
+			fmt.Println(msg)
+			return
+		}
+		token, refreshtoken, _ := generate.TokenGenerator(*founduser.Email, *founduser.First_Name, *founduser.Last_Name, *founduser.User_ID)
+		defer cancel()
+
+		generate.UpdateAllTokens(token, refreshtoken, founduser.User_ID)
+
+		c.JSON(http.StatusFound, founduser)
+	}
 
 }
 
