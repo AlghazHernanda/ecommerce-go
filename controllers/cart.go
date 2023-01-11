f273df1c2043fbd93dd2846992b0e4a3026858a6
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-01-11 20:09:25 +0700

    update cart

--- controllers/cart.go
+++ controllers/cart.go
@@ -1,6 +1,43 @@
 package controllers
 
+import (
+	"errors"
+	"log"
+	"net/http"
+)
+
+type Application struct {
+	prodCollection *mongo.Collection
+	userCollection *mongo.Collection
+}
+
+func NewApplication(prodCollection, userCollection *mongo.Collection) *Application {
+	return &Application{
+		prodCollection: prodCollection,
+		userCollection: userCollection,
+	}
+}
+
 func (app *Application) AddToCart() gin.Handler {
+	return func(c *gin.Context) {
+		productQueryID := c.Query("id")
+		if productQueryID == "" {
+			log.Println("product id is empty")
+
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("product id is empty"))
+			return
+
+		}
+
+		userQueryID := c.Query("user_id")
+		if userQueryID == "" {
+			log.Println("user id is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("user id is empty"))
+			return
+		}
+
+		productID, err := primitive.ObjectIDFromHex(productQueryID)
+	}
 }
 
 func (app *Application) RemoveItem() gin.HandlerFunc {
