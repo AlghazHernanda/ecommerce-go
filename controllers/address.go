3b11d32bf657e609ee3221fa1406793c4cb4a339
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-01-13 11:44:02 +0700

    add deleteadress

--- controllers/address.go
+++ controllers/address.go
@@ -1,6 +1,12 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/AcevedoEsteban/goEcommerce-yt/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 func AddAddresss() gin.HandlerFunc {
 
@@ -15,5 +21,24 @@ func EditWorkAddress() gin.HandlerFunc {
 }
 
 func DeleteAddress() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		user_id := c.Query("id")
+
+		if user_id == "" {
+			c.Header("Context-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"Error": "Invalid Search Index"})
+			c.Abort()
+			return
+		}
+
+		addresses := make([]models.Address, 0)
+
+		usert_id, err := primitive.ObjectIDFromHex(user_id)
+
+		if err != nil {
+			c.IndentedJSON(500, "InternaL Server Error")
+		}
+
+	}
 
 }
