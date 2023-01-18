38b864d421dc44824e68c97bd4b500c2c7b19aed
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-01-18 11:24:40 +0700

    adding add to cart

--- database/cart.go
+++ database/cart.go
@@ -18,7 +18,31 @@ var (
 func AddProductToCart(ctx context.Context, prodCollection, userCollection *mongo.Collection, productId primitive.ObjectID, userId string) error {
 	searchfromdb, err := prodCollection.Find(ctx, bson.M{"_id": productId})
 
-	
+	if err != nil {
+		log.Println(err)
+		return ErrCantFindProduct
+	}
+	var productCart []models.ProductUser
+	err = searchfromdb.All(ctx, &productCart)
+	if err != nil {
+		log.Println(err)
+		return ErrCantDecodeProducts
+	}
+	id, err := primitive.ObjectIDFromHex(userId)
+
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdIsNotValid
+	}
+
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+	update := bson.D{{Key: "$push", Value: bson.D{primitive.E{Key: "usercart", Value: bson.D{{Key: "$earch", Value: productCart}}}}}}
+
+	_, err = userCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return ErrCantUpdateUser
+	}
+	return nil
 }
 
 func RemoveCartItem(){
