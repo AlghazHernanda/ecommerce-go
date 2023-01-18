bfbba2b587635297aa42663f80268c3966b00c80
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-01-18 11:24:54 +0700

    add middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,8 +1,27 @@
 package middleware
 
 import (
+	"net/http"
+
+	token "gihtub.com/SherzodAbdullajonov/ecommerce-yt/tokens"
 	"github.com/gin-gonic/gin"
 )
 
 func Authentication() gin.HandlerFunc {
+	ClientToken := ctx.Request.Header.Get("token")
+	if ClientToken == "" {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "no Authorizaton header"})
+		ctx.Abort()
+		return
+	}
+	claims, err := token.ValidateToken(ClientToken)
+	if err != "" {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		ctx.Abort()
+		return
+	}
+
+	ctx.Set("email", claims.Email)
+	ctx.Set("uid", claims.Uid)
+	ctx.Next()
 }
